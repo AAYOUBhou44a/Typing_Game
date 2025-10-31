9ccf9914fbf65399645bc02e0afeb23f1c50bea4
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2025-10-31 19:51:00 +0100

    l'ajout du chronomètre qui limite le temps de jeux, et du message de performance

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const texteDiv = document.querySelector(".texte");
 let input = document.querySelector(".input");
 let conteur = 0;
 const spans = [];
+let tempsRestant = 60;
+let correct = 0;
+let incorrect = 0;
+let timer = null;
 for(let i=0;i<texte.length;i++)
 {
     const span = document.createElement("span");
@@ -15,6 +19,8 @@ for(let i=0;i<texte.length;i++)
 
 }
 input.addEventListener("input", () => {
+    correct = 0;
+    incorrect = 0;
     const inputValue = input.value;
     spans.forEach((span, index) => {
         if(inputValue[index]==null){
@@ -22,16 +28,43 @@ input.addEventListener("input", () => {
         }
         else if(inputValue[index]==span.innerText){
             span.style.color = "green";
+            correct++;
         }
-        else{span.style.color = "red";}
+        else{
+            span.style.color = "red";
+            incorrect++;
+        }
     });
-    // if(inputValue===spans[conteur].innerText)
-    // {
-    //     spans[conteur].style.color = "green";
-    // }
-    // else{document.span.color = "red";}
-    // conteur ++;
 });
+let temps = document.querySelector(".temps");
+
+input.addEventListener("input", function(){
+    if(!timer){
+    timer = setInterval(() => {
+        tempsRestant--;
+        temps.innerText = `le temps restant est : ${tempsRestant} `;
+        if(tempsRestant <= 0 || correct==texte.length){
+            finDeJeu();
+        }
+    }, 1000);
+              }
+});
+
+function finDeJeu(){
+        input.disabled = true;
+        clearInterval(timer);
+        temps.innerText = "le jeux a terminé!";
+        let minutes = (60-tempsRestant)/60;
+        let wpm = Math.round((correct/5)/minutes);
+        let accurancy = (correct/(correct+incorrect))*100;
+        const resultat = document.querySelector(".resultat");
+        resultat.innerText = `Bravo! tu peut écrire ${wpm} mots par minute , ton précision est de ${accurancy}% `;
+        resultat.style.background = "rgb(19, 120, 215)";
+}
+
+
+
+
 
 
 
